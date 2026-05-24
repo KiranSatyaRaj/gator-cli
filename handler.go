359d47e7c835c66285d1ea4bc0d8af0d8f11f6f7
KiranSatyaRaj <kiranjamy20021508@gmail.com>
2026-05-24 22:52:36 +0530

    add db and additional cmds

--- handler.go
+++ handler.go
@@ -1,15 +1,69 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/KiranSatyaRaj/gator-cli/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(state *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return errors.New("expected username, got zero args")
 	}
-	state.cfg.SetUser(cmd.args[0])
+	user, err := state.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		os.Exit(1)
+	}
+	state.cfg.SetUser(user.Name)
 	fmt.Print("user id created\n")
+	return nil
+}
+
+func handlerRegister(state *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("expected username, got zero args")
+	}
+	userInfo := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+	}
+	user, err := state.db.CreateUser(context.Background(), userInfo)
+	if err != nil {
+		os.Exit(1)
+	}
+	state.cfg.SetUser(user.Name)
+	fmt.Printf("Registered %v as user at %v\n", user.Name, user.CreatedAt)
+	return nil
+}
+
+func handlerReset(state *state, cmd command) error {
+	if err := state.db.DeleteAllUsers(context.Background()); err != nil {
+		log.Fatal("Unable to reset users")
+		return err
+	}
+	log.Println("Deleted All users")
+	return nil
+}
+
+func handlerUsers(state *state, cmd command) error {
+	users, err := state.db.GetUsers(context.Background())
+	if err != nil {
+		log.Fatal("No users found")
+	}
+	for _, user := range users {
+		if user == state.cfg.CurrentUserName {
+			fmt.Printf("%s (current)\n", user)
+		} else {
+			fmt.Println(user)
+		}
+	}
 	return nil
 }
--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 
 	"github.com/KiranSatyaRaj/gator-cli/internal/config"
+	"github.com/KiranSatyaRaj/gator-cli/internal/database"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	args := os.Args
-	if len(args) <= 2 {
+	if len(args) < 2 {
 		fmt.Errorf("Invalid num of args, expected atleast two")
 		os.Exit(1)
 	}
@@ -17,10 +20,25 @@ func main() {
 	st := &state{}
 	cfg := config.Read()
 	st.cfg = &cfg
+	db, err := sql.Open("postgres", st.cfg.DB_URL)
+	if err != nil {
+		panic(err)
+	}
+	dbQueries := database.New(db)
+	st.db = dbQueries
 	cmds := commands{}
 	cmds.cmds = make(map[string]func(*state, command) error)
 	cmd := command{args[1], args[2:]}
-	cmds.register(args[1], handlerLogin)
+	switch args[1] {
+	case "login":
+		cmds.register("login", handlerLogin)
+	case "register":
+		cmds.register("register", handlerRegister)
+	case "reset":
+		cmds.register("reset", handlerReset)
+	case "users":
+		cmds.register("users", handlerUsers)
+	}
 	if err := cmds.run(st, cmd); err != nil {
 		panic(err)
 	}
